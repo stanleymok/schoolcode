c312d4a0eebc88e2a381e83fc78ed6dc7972e0dc
stanleymok <stanleyschoolwork@gmail.com>
2019-03-22 21:03:07 +0800

    add placeholder Python class for Python OOP syntax revision

---
+++ Human.py
@@ -0,0 +1,11 @@
+#placeholder class for python syntax revision (:
+class Human:
+
+    #constructor
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    #placeholder method
+    def toString(self):
+        return self.name
